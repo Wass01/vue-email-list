57d83dcda3a010d17d5d0f58274d2a6bfa8b1b66
wass01 <maatallawassim01@gmail.com>
2021-04-08 14:14:50 +0100

    creazione file

---
+++ main.js
@@ -0,0 +1,15 @@
+// Attraverso l'apposita API di Boolean
+// https:flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli
+// in pagina all'interno di una lista.
+
+
+let app = new Vue({
+  el: "#root",
+  data: {
+
+  },
+
+  methods: {
+    }
+});
