ea70e058013a6ac9f1e3b6bab4fae002d8a2e6db
wass01 <maatallawassim01@gmail.com>
2021-04-08 15:06:02 +0100

    esercizio terminato

--- main.js
+++ main.js
@@ -7,9 +7,17 @@
 let app = new Vue({
   el: "#root",
   data: {
-
+    email: [],
   },
 
   methods: {
-    }
+    genEmail: function() {
+      for (var i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+          this.email.push(response.data.response);
+        });
+      }
+    },
+  }
 });
